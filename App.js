db5181e4509feaa3e8c71950ba7e2909bb896c4f
Giorgix <jackosinapsis@gmail.com>
2018-05-28 18:03:59 -0400

    removed unnecessary components

--- App.js
+++ App.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import RouterNav from './components/RouterNav'
-import TabBar from './components/TabBar'
-import Navigator from './components/Navigator'
 
 import { reducer } from './redux/locationsRedux'
 const store = createStore(reducer)
